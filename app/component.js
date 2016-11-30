a978990907f41d19e0b887deadae1ed23885a1be
Danial Moazen <dmoazen@fandango.com>
2016-11-30 12:16:04 -0800

    css is home

--- app/component.js
+++ app/component.js
@@ -1,7 +1,10 @@
+var styles = require('./main.css');
+
 module.exports = function () {
   var element = document.createElement('h1');
 
   element.innerHTML = 'If you don\'t eat your meat, you can\'t have any pudding!';
+  element.className = styles.bg;
 
   return element;
 };
